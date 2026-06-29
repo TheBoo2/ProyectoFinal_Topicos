559eabc81aa7ce86a2bd0976dcd3107440f53336
Jose <jocentel90@gmail.com>
2026-06-29 07:52:15 +0000

    Corregido formato estetico de network.tf para el pipeline

--- infra/network.tf
+++ infra/network.tf
@@ -9,7 +9,7 @@ resource "azurerm_subnet" "aci" {
   name                 = "${var.prefix}-subnet-aci"
   resource_group_name  = data.azurerm_resource_group.main.name
   virtual_network_name = azurerm_virtual_network.main.name
-  address_prefixes      = ["10.0.1.0/24"]
+  address_prefixes     = ["10.0.1.0/24"]
 
   delegation {
     name = "aci-delegation"
