a90b4af8def8d52282cd9f62158bbf46a7b28705
Jose <jocentel90@gmail.com>
2026-06-29 05:57:23 +0000

    Infraestructura Pipeline 1: VNet, subred, ACR y ACI funcionando en eastus2

--- infra/network.tf
+++ infra/network.tf
@@ -1,1 +1,21 @@
-# VNet y subred
+resource "azurerm_virtual_network" "main" {
+  name                = "${var.prefix}-vnet"
+  address_space       = ["10.0.0.0/16"]
+  location            = var.location
+  resource_group_name = data.azurerm_resource_group.main.name
+}
+
+resource "azurerm_subnet" "aci" {
+  name                 = "${var.prefix}-subnet-aci"
+  resource_group_name  = data.azurerm_resource_group.main.name
+  virtual_network_name = azurerm_virtual_network.main.name
+  address_prefixes      = ["10.0.1.0/24"]
+
+  delegation {
+    name = "aci-delegation"
+    service_delegation {
+      name    = "Microsoft.ContainerInstance/containerGroups"
+      actions = ["Microsoft.Network/virtualNetworks/subnets/action"]
+    }
+  }
+}
--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,1 +1,11 @@
-# Outputs
+output "acr_login_server" {
+  value = azurerm_container_registry.main.login_server
+}
+
+output "aci_private_ip" {
+  value = azurerm_container_group.main.ip_address
+}
+
+output "vnet_id" {
+  value = azurerm_virtual_network.main.id
+}
